37279656b5d0502a9c71602c00211becf2b8237b
Manisha <manisha.wenappdev@gmail.com>
2021-07-19 10:47:58 +0530

    Update iui edit blade

--- resources/views/admin/iui/edit1.blade.php
+++ resources/views/admin/iui/edit1.blade.php
@@ -3838,7 +3838,7 @@ $dose =  ['' => 'Select Dose','1'=>'Daily','2'=>"Once a week",'3'=>"Twice a week
                                     <div class="input-group">
                                         <span class="input-group-addon">Follow Up: &nbsp;</span>
                                         @if(!empty($historyPlan->follow_up))
-                                            {{Form::text("follow_up",!empty($historyPlan->follow_up) ? \Carbon\Carbon::parse($historyPlan->follow_up)->format('D d M Y') : null,['class'=>'form-control datetimepicker follow-up-date','disabled'])}}
+                                            {{Form::text("follow_up",!empty($historyPlan->follow_up) ? \Carbon\Carbon::parse($historyPlan->follow_up)->format('D d M Y') : null,['class'=>'form-control datetimepicker follow-up-date'])}}
                                             {{Form::hidden('data[plan][follow_up]',$historyPlan->follow_up)}}
                                             {{Form::hidden('data[new_follow_up]',$historyPlan->follow_up)}}
                                         @else

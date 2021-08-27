0352d74b1e5fe73547d85bae203fa1a1b665e6ba
Manisha <manisha.wenappdev@gmail.com>
2021-08-27 12:40:47 +0530

    Update HtmlPages view

--- resources/views/admin/html_pages/preview.blade.php
+++ resources/views/admin/html_pages/preview.blade.php
@@ -1,14 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Page Title</title>
+<title></title>
 <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style>
+    img{
+      margin: 0px 0px 0px 40px;
+    }
+    body
+    {
+      text-align: justify; 
+      padding:20px;
+    }
+  </style>
 </head>
-<body style="text-align: justify; padding:20px;">
+<body>
 
 @php
     $data = !empty($html_page->description) ? $html_page->description : '';
